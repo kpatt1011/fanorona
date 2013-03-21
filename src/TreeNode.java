6fd125caf40b8ebe8cf7ac496ef536fb19607df8
Travis Lamb <travis.t.lamb@gmail.com>
2013-03-20 19:00:52 -0500

    Completed creation and traversal of minimax tree

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,18 +1,49 @@
 import java.util.*;
 
-class MinNode
+interface MinimaxNode
 {
+    void populateChildren (int depth);
+
+    ArrayList<? extends MinimaxNode> getChildren ();
+
+    public Vector<Vector<Point>> getGameBoard ();
+
+    boolean isLeaf ();
+
+    int value ();
+
+    public boolean isMaxNode ();
+    public boolean isMinNode ();
+}
+
+class MinNode implements MinimaxNode
+{
+    // DATA MEMBERS
 	private ArrayList<MaxNode> children;
+    private Vector<Vector<Point>> gameBoard = new Vector<Vector<Point>>();
 	
-	public MinNode ()
+    // PUBLIC FUNCTIONS
+	public MinNode (Vector<Vector<Point>> newGameBoard)
 	{
-		
+		gameBoard = newGameBoard;
 	}
 	
-	public void addChild (MaxNode newChild)
-	{
-		children.add(newChild);
-	}
+    public void populateChildren (int depth)
+    {
+        // get all possible board states from current moves
+        // iterate across all of them and addChild()
+        // if depth > 0, call populateChildren(depth - 1) on all children
+    }
+
+    public ArrayList<MaxNode> getChildren ()
+    {
+        return children;
+    }
+
+    public Vector<Vector<Point>> getGameBoard ()
+    {
+        return gameBoard;
+    }
 	
 	public boolean isLeaf ()
 	{
@@ -37,21 +68,53 @@ class MinNode
 			return value;
 		}
 	}
-}
 
-class MaxNode
-{
-	private ArrayList<MinNode> children;
-	
-	public MaxNode ()
-	{
-		
-	}
-	
-	public void addChild (MinNode newChild)
+    public boolean isMaxNode ()
+    {
+        return false;
+    }
+
+    public boolean isMinNode ()
+    {
+        return true;
+    }
+
+    // HELPER FUNCTIONS
+	private void addChild (MaxNode newChild)
 	{
 		children.add(newChild);
 	}
+
+}
+
+class MaxNode implements MinimaxNode
+{
+    // DATA MEMBERS
+	private ArrayList<MinNode> children;
+    private Vector<Vector<Point>> gameBoard = new Vector<Vector<Point>>();
+	
+    // PUBLIC FUNCTIONS
+	public MaxNode (Vector<Vector<Point>> newGameBoard)
+	{
+        gameBoard = newGameBoard;
+    }
+	
+    public void populateChildren (int depth)
+    {
+        // get all possible board states from current moves
+        // iterate across all of them and addChild()
+        // if depth > 0, call populateChildren(depth - 1) on all children
+    }
+	
+    public ArrayList<MinNode> getChildren ()
+    {
+        return children;
+    }
+	
+    public Vector<Vector<Point>> getGameBoard ()
+    {
+        return gameBoard;
+    }
 	
 	public boolean isLeaf ()
 	{
@@ -75,5 +138,21 @@ class MaxNode
 			
 			return value;
 		}
+	}
+
+    public boolean isMaxNode ()
+    {
+        return true;
+    }
+
+    public boolean isMinNode ()
+    {
+        return false;
+    }
+
+    // HELPER FUNCTIONS
+	private void addChild (MinNode newChild)
+	{
+		children.add(newChild);
 	}
 }
