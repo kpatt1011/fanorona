2a164c1983238b16059b6b9b79a09292a7abbb97
RYAN WILLIAM COVERICK <rcoverick@gmail.com>
2013-03-31 21:38:05 -0500

    print welcome message and wait for 'ready'
    sets up default board for server, sends info over socket to the client,
    waits for the client to reply with 'READY' before beginning the game

--- src/SocketMain.java
+++ src/SocketMain.java
@@ -17,20 +17,39 @@ public class SocketMain {
 		/*run through entire game in this function, operating as the server for the game*/
 		/**/
 		/*parse string from input*/
+		byte[] buffer = new byte[255];
+		//char[] bufferToString = new char[255];
 		try { 
 			ServerSocket server = new ServerSocket(socketNumber);
 			System.out.println("listening to port 5000...");
 			Socket s = server.accept();
 			InputStream socketInput = s.getInputStream();
 			OutputStream socketOutput = s.getOutputStream();
-			
+			PrintStream messageStream = new PrintStream(socketOutput);
+			/*send welcome message to the client 
+			 * default board dimensions will be used so default board
+			 * constructor is used for server model*/
 			FanoronaGameBoard fg = new FanoronaGameBoard();
-			while (!fg.isGameOver())
+			messageStream.print("WELCOME\n");
+			messageStream.flush();
+			messageStream.print("INFO 9 5 B 5000\n");
+			messageStream.flush();
+			/* convert read bytes into a string 
+			 * keep reading bytes from input stream until "READY" message is received*/
+			String clientResponse = new String(buffer);
+			while(!clientResponse.equals("READY")||!clientResponse.equals("ready"))
 			{
-				/*send and receive messages over the socket until the game is over*/
+				socketInput.read(buffer,0,6);
+				clientResponse = new String(buffer);
 			}
 			
-			
+			/*send and receive messages over the socket until the game is over*/
+			while (!fg.isGameOver())
+			{
+				
+				
+			}
+
 			server.close();
 			System.out.println("connection terminated");
 
@@ -48,7 +67,7 @@ public class SocketMain {
 		String gameType="";
 		InputStreamReader inputStream= new InputStreamReader(System.in);
 		BufferedReader in = new BufferedReader(inputStream);
-		out.println("Welcome to Fanorona!\nPlease designate this game" +
+		System.out.println("Welcome to Fanorona!\nPlease designate this game" +
 				" to be either a client or a server\n");
 		try{
 		gameType=in.readLine();
